6aa9389cfe13ca91b27f450f033a12d5b47dad65
JuanfeMon23 <ELFuanfex@hotmail.com>
2023-08-01 21:56:15 -0500

    database conection, app workig with express and index running the server

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import app from './app.js'
+import { sequelize } from './database/database.js';
 
-app.listen(3000);
+
+async function main(){
+    try {
+        await sequelize.sync();
+        app.listen(3000);
+        console.log('Server is running on port 3000');
+    } catch (error) {
+        console.log('Cannot connect to server')
+    }
+}
+
+main();
